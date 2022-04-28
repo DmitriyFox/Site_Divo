728cd34f82f7c823501bc5fcdad66542afdac5b8
DmitriyFox <gazpress_dima@mail.ru>
2022-04-28 13:28:05 +0500

    Continue make slider Slickslider.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-if (window.jQuery) {
-    var verJquery = jQuery.fn.jquery;
-    // выведем версию jQuery в консоль
-    console.log(verJquery);
-}
+$('.main_slider').slick({
+    Infinite: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+});
+
+// if (window.jQuery) {
+//     var verJquery = jQuery.fn.jquery;
+//     // выведем версию jQuery в консоль
+//     console.log(verJquery);
+// }
+
+// if (window.jQuery) {
+//     // jQuery подключен к странице
+//   } else {
+//     // jQuery не подключен к странице
+//   }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     /* код на jQuery */
+//     // установим body фон #eee
+//     $('body').css('background-color', '#eee');
+//     // выровняем h1 по центру
+//     $('h1').css('text-align', 'center');
+//     // добавим в конец страницы элемент div с контентом "Я люблю jQuery"
+//     $('body').append('<div style="text-align: center; font-size: 20px;">Я люблю jQuery</div>');
+//   });
